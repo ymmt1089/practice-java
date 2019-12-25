1342f20a5a6be0d2bd9954f891d118fbeca4f7ff
ymmt1089 <ymmt1089@gmail.com>
2019-12-25 17:19:06 +0900

    テスト

---
+++ 0/Main.java
@@ -0,0 +1,13 @@
+class Sample {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+
+
