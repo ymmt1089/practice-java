7564a2c9afcd4ce203f18540bd0dad4a8813d6ed
ymmt1089 <ymmt1089@gmail.com>
2019-12-25 20:29:49 +0900

    問題

--- 0/Main.java
+++ 0/Main.java
@@ -1,13 +1,12 @@
-class Sample {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
+// class Sample {
+//     public static void main(String[] args){
+//         System.out.println("Hello World!");
+//     }
+// }
 
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println(3+3);
     }
 }
-
-
