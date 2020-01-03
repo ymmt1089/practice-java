bf073220bac62ef21a266d91d08dd4aaa7a9a76a
ymmt1089 <ymmt1089@gmail.com>
2020-01-03 19:31:00 +0900

    演算子

--- 0/Mondai.java
+++ 0/Mondai.java
@@ -1,15 +1,15 @@
-public class Mondai{
-    public static void main(String[] args){
-        int tate = 3;
-        int yoko = 5;
-        System.out.print("縦幅"+tate+"横幅"+yoko+"の長方形の面積は"+(tate*yoko));
+// public class Mondai{
+//     public static void main(String[] args){
+//         int tate = 3;
+//         int yoko = 5;
+//         System.out.print("縦幅"+tate+"横幅"+yoko+"の長方形の面積は"+(tate*yoko));
 
 
-        boolean a = true;
-        char b = '翔';
-        double c = 3.14;
-        long d  = 314159265853979L;
-        String e = "ああああ";
-        System.out.println(c+d);
-    }
-}
+//         boolean a = true;
+//         char b = '翔';
+//         double c = 3.14;
+//         long d  = 314159265853979L;
+//         String e = "ああああ";
+//         System.out.println(c+d);
+//     }
+// }
