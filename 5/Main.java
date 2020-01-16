5442f1beb1e4fc217ee8aa2ab86a1da69c646077
ymmt1089 <ymmt1089@gmail.com>
2020-01-16 15:52:23 +0900

    オーバーロード

--- 5/Main.java
+++ 5/Main.java
@@ -60,12 +60,52 @@
 // }
 
 
+// public class Main{
+//     public static int add(int x,int y){
+//         int ans = x + y;
+//         return ans;
+//     }
+//     public static void main(String[] args){
+//         System.out.println(add(add(10,20),add(30,40)));//add(30,70)と同意。戻り値をそのまま使いこともできる
+//     }
+// }
+
+
+
+
+// ここからオーバーロード（メソッド名の多重定義）
+// 仮引数（以下のaddの前につくintやdouble）が異なればOK
+// public class Main{
+//     // １つめadd
+//     public static int add(int x,int y){
+//         return x + y;
+//     }
+//     // ２つめadd
+//     public static double add(double x,double y){
+//         return x + y;
+//     }
+//     // ３つめadd
+//     public static String add(String x, String y){
+//         return x + y;
+//     }
+//     public static void main(String[] args){
+//         System.out.println(add(10,20));
+//         System.out.println(add(3.5,2.7));
+//         System.out.println(add("Helo","World"));
+//     }
+// }
+
+
+
 public class Main{
     public static int add(int x,int y){
-        int ans = x + y;
-        return ans;
+        return x + y;
+    }
+    public static int add(int x,int y,int z){
+        return x + y + z;
     }
     public static void main(String[] args){
-        System.out.println(add(add(10,20),add(30,40)));//add(30,70)と同意。戻り値をそのまま使いこともできる
+        System.out.println(add(10,20));
     }
 }
+// こういう風に引数の数でも判断できる
